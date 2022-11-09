20f152a5c603216784bfea206880d331e04518a2
kvaillant <vaillant.karl.971@gmail.com>
2022-11-09 23:49:40 +0100

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,8 @@ import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
 dotenv.config();
 import "./tasks/index";
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -14,6 +16,10 @@ const config: HardhatUserConfig = {
     },
     hardhat: {
       chainId: 31337,
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
     },
   },
   etherscan: {
